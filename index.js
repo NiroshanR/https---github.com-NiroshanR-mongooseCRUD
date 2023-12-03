0cecb31fdbd7720e2165377aaac46d18c540ea15
NiroshanR <niroshanragu@gmail.com>
2023-12-03 12:19:52 +0530

    added not found page

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.set('view engine' , 'ejs')
 
 
 
-const variable = [
+const USERS = [
     { 
     username : 'ram',
     firstName : 'Ram', 
@@ -42,36 +42,59 @@ app.get('/', (req,res) => {
     res.json({message : 'All ok works good!'})
 });
 
-app.get('/:username', (req,res) => {
-    console.log(req.params)
-    res.send('hi')
+// Creating dynamic routes to use same routes and template to handle different pages
+// app.get('/:username', (req,res) => { // :username is params
+//     console.log(req.params) // see the output in terminal
+//     res.send('hi')
+// });
+
+// app.get('/:username/:test', (req,res) => { // :username is params
+//     console.log(req.params) // see the output in terminal
+//     res.send('hi')
+// });
+
+//404 NOT FOUND Page 
+app.get('/not-found', (req,res) => {
+    res.render('not-found')
 });
 
-
-app.get('/ram', (req,res) => {
-    let ramDetails = { 
-        firstName : 'Ram', 
-        lastName : 'kumar', 
-        avatar : 'https://reqres.in/img/faces/7-image.jpg' , 
-        email : 'ramkumar@gmail.com',
-        isPremium : true,
-        hobbies : ['I' , 'J' , ' K' ]
+app.get('/:username', (req,res) => { // :username is params
+    const {username } = req.params
+    const userDetails = USERS.find(user => user.username === username)
+    if(userDetails){
+        res.render('user', userDetails)
     }
-    res.render('user', ramDetails)
+    else res.redirect('not-found')
+    
 });
 
 
-app.get('/lakshman', (req,res) => {
-    let lakshmanDetails = { 
-        firstName : 'Lakshman', 
-        lastName : 'Singh' , 
-        avatar : 'https://reqres.in/img/faces/8-image.jpg', 
-        email : 'lakshmansingh@gmail.com',
-        isPremium : false,
-        hobbies : ['I' , 'J' , ' K' ]
-    }
-    res.render('user', lakshmanDetails )
-});
+
+// Dynamic pages
+// app.get('/ram', (req,res) => {
+//     let ramDetails = { 
+//         firstName : 'Ram', 
+//         lastName : 'kumar', 
+//         avatar : 'https://reqres.in/img/faces/7-image.jpg' , 
+//         email : 'ramkumar@gmail.com',
+//         isPremium : true,
+//         hobbies : ['I' , 'J' , ' K' ]
+//     }
+//     res.render('user', ramDetails)
+// });
+
+
+// app.get('/lakshman', (req,res) => {
+//     let lakshmanDetails = { 
+//         firstName : 'Lakshman', 
+//         lastName : 'Singh' , 
+//         avatar : 'https://reqres.in/img/faces/8-image.jpg', 
+//         email : 'lakshmansingh@gmail.com',
+//         isPremium : false,
+//         hobbies : ['I' , 'J' , ' K' ]
+//     }
+//     res.render('user', lakshmanDetails )
+// });
 
 // app.get('/user', (req,res) => {
 //     res.render('user')
